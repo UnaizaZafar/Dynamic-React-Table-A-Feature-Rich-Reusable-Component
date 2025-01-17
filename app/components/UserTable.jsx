08559212a2b42f9c09f4190e12c8791514d51032
Unaiza08 <i.unaiza.aginfotech@gmail.com>
2025-01-17 17:50:56 +0500

    feat(pagination): shows 5 pages at a time

--- app/components/UserTable.jsx
+++ app/components/UserTable.jsx
@@ -1,33 +1,22 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const UserTable = ({ userData, tableHeading, columns }) => {
   const data = Array.isArray(userData) ? userData : userData.data;
   const [currentPage, setCurrentPage] = useState(1); //to be at the 1st page when page loads
-  // const rowsPerPage = 6;
   const [rowsPerPage, setRowsPerPage] = useState(6);
   const lastIndex = currentPage * rowsPerPage; //getting the index of last row on selected page
   const startIndex = lastIndex - rowsPerPage;
   const numberOfPages = Math.ceil(data.length / rowsPerPage); // divide total data with rows to get number of pages on which the data is divided
   const records = data.slice(startIndex, lastIndex);
-  function nextPage() {
-    if (currentPage != numberOfPages) {
-      // if its not on the last page then move forwards
-      setCurrentPage((prev) => prev + 1);
-    }
-  }
-
-  function prevPage() {
-    if (currentPage != 1) {
-      setCurrentPage((prev) => prev - 1);
-    }
-  }
-
+  // const [pageNumberLimit, setpageNumberLimit] = useState(5);
+  const [maxPageNumberLimit, setmaxPageNumberLimit] = useState(5);
+  const [minPageNumberLimit, setminPageNumberLimit] = useState(0);
   return (
     <div className="mx-auto w-full max-w-4/5 rounded-lg px-8 py-6 text-black bg-white border border-zinc-200">
       <h1 className="text-3xl text-cyan-800 font-bold  pb-4">{tableHeading}</h1>
       <div className="flex gap-2 mb-2 items-center justify-end">
-        <h3 className="font-medium">Number of Rows: </h3>
+        <h3 className="font-medium text-sm">Number of Rows: </h3>
         <input
           type="number"
           value={rowsPerPage}
@@ -90,48 +79,136 @@ const UserTable = ({ userData, tableHeading, columns }) => {
           ))}
         </tbody>
       </table>
-      <div className="flex gap-3 mt-4 justify-end">
+
+      {/* PAGINATION */}
+      <div className="flex gap-3 mt-4 justify-end items-center">
+        {/* Got to first */}
+        <button
+          onClick={() => {
+            setCurrentPage(1);
+          }}
+          className={`size-8 border rounded border-gray-300 p-1 hover:bg-cyan-800 hover:text-white justify-items-center  ${
+            currentPage === 1
+              ? "hover:bg-white !text-black/50 cursor-not-allowed"
+              : ""
+          }`}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            width={24}
+            height={24}
+            strokeWidth={2}
+          >
+            <path d="M11 7l-5 5l5 5" />
+            <path d="M17 7l-5 5l5 5" />
+          </svg>
+        </button>
         {/* previous */}
-        <svg
+        <button
           onClick={() => {
             currentPage != 1 && setCurrentPage((prev) => prev - 1);
+            // (currentPage - 1) % pageNumberLimit == 0 &&
+            //   setmaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit) &&
+            //   setminPageNumberLimit(minPageNumberLimit - pageNumberLimit);
           }}
-          xmlns="http://www.w3.org/2000/svg"
-          shapeRendering="geometricPrecision"
-          textRendering="geometricPrecision"
-          imageRendering="optimizeQuality"
-          fillRule="evenodd"
-          clipRule="evenodd"
-          className="size-5 rotate-180 cursor-pointer"
-          viewBox="0 0 299 511.517"
+          className={`size-8 border rounded border-gray-300 p-1 hover:bg-cyan-800 hover:text-white justify-items-center ${
+            currentPage === 1
+              ? "hover:bg-white !text-black/50 cursor-not-allowed"
+              : ""
+          }`}
         >
-          <path
-            fill="#000"
-            d="M12.579 436.254c-36.893 64.703 15.581 96.095 51.926 60.146L273.92 295.723c33.44-33.439 33.44-46.49 0-79.93L64.505 15.117C28.16-20.832-24.314 10.56 12.579 75.262l97.261 180.496-97.261 180.496z"
-          />
-        </svg>
-        <h3>
-          {currentPage}/{numberOfPages}
-        </h3>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            width={24}
+            height={24}
+            className="rotate-180"
+            strokeWidth={2}
+            strokeLinejoin="round"
+            strokeLinecap="round"
+            stroke="currentColor"
+          >
+            <path d="M9 6l6 6l-6 6" />
+          </svg>
+        </button>
+        {/* Page Numbers */}
+
+        {Array.from({ length: numberOfPages }, (_, key) => key + 1).map(
+          (pageNumber) =>
+            pageNumber <= maxPageNumberLimit &&
+            pageNumber > minPageNumberLimit && (
+              <button
+                onClick={() => setCurrentPage(pageNumber)}
+                key={pageNumber}
+                className={` size-8 border rounded border-gray-300 p-1 hover:bg-cyan-800 hover:text-white justify-items-center ${
+                  currentPage === pageNumber ? "bg-cyan-800 text-white" : ""
+                } `}
+              >
+                {pageNumber}
+              </button>
+            )
+        )}
         {/* next */}
-        <svg
+        <button
           onClick={() => {
             currentPage != numberOfPages && setCurrentPage((prev) => prev + 1);
+            // currentPage + 1 > maxPageNumberLimit
+            //   ? setmaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit)
+            //   : ""
+            //   ? setminPageNumberLimit(minPageNumberLimit + pageNumberLimit)
+            //   : "";
           }}
-          xmlns="http://www.w3.org/2000/svg"
-          shapeRendering="geometricPrecision"
-          textRendering="geometricPrecision"
-          imageRendering="optimizeQuality"
-          fillRule="evenodd"
-          clipRule="evenodd"
-          className="size-5 cursor-pointer"
-          viewBox="0 0 299 511.517"
+          className={`Fsize-8 border rounded border-gray-300 p-1 hover:bg-cyan-800 hover:text-white justify-items-center cursor-pointer ${
+            currentPage === numberOfPages
+              ? "hover:bg-white !text-black/50 cursor-not-allowed"
+              : ""
+          }`}
         >
-          <path
-            fill="#000"
-            d="M12.579 436.254c-36.893 64.703 15.581 96.095 51.926 60.146L273.92 295.723c33.44-33.439 33.44-46.49 0-79.93L64.505 15.117C28.16-20.832-24.314 10.56 12.579 75.262l97.261 180.496-97.261 180.496z"
-          />
-        </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            width={24}
+            height={24}
+            strokeWidth={2}
+            strokeLinejoin="round"
+            strokeLinecap="round"
+            stroke="currentColor"
+          >
+            <path d="M9 6l6 6l-6 6" />
+          </svg>
+        </button>
+        {/* Go to Last */}
+        <button
+          onClick={() => setCurrentPage(numberOfPages)}
+          className={`size-8 border rounded border-gray-300 p-1 hover:bg-cyan-800 hover:text-white justify-items-center ${
+            currentPage === numberOfPages
+              ? "hover:bg-white !text-black/50 cursor-not-allowed"
+              : ""
+          }`}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            width={24}
+            height={24}
+            className="rotate-180"
+            strokeWidth={2}
+          >
+            <path d="M11 7l-5 5l5 5" />
+            <path d="M17 7l-5 5l5 5" />
+          </svg>
+        </button>
       </div>
     </div>
   );
