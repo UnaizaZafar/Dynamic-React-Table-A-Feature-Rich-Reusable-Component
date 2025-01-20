e944286a1443aef903c47024512a8f5459e8ec74
Unaiza08 <i.unaiza.aginfotech@gmail.com>
2025-01-20 15:17:51 +0500

    refactor: Chevron icon updated

--- app/components/UserTable.jsx
+++ app/components/UserTable.jsx
@@ -57,7 +57,7 @@ const UserTable = ({ userData, tableHeading, columns }) => {
                         className="size-6"
                         viewBox="0 0 64 64"
                       >
-                        <path d="M27 55L6 33 9 29 26 41 55 12 59 16z"></path>
+                        <path d="M27 55L6 33 9 29 26 41 55 12 59 18z"></path>
                       </svg>
                     ) : (
                       // cross
@@ -70,7 +70,7 @@ const UserTable = ({ userData, tableHeading, columns }) => {
                         className="size-6"
                         viewBox="0 0 64 64"
                       >
-                        <path d="M 12 8 L 8 12 L 24.666016 32 L 8 52 L 12 56 L 32 39.333984 L 52 56 L 56 52 L 39.333984 32 L 56 12 L 52 8 L 32 24.666016 L 12 8 z"></path>
+                        <path d="M 12 8 L 8 12 L 24.666018 32 L 8 52 L 12 56 L 32 39.333984 L 52 56 L 56 52 L 39.333984 32 L 56 12 L 52 8 L 32 24.666018 L 12 8 z"></path>
                       </svg>
                     )}
                   </td>
@@ -90,8 +90,7 @@ const UserTable = ({ userData, tableHeading, columns }) => {
               onClick={() => setIsOpen(!isOpen)}
             >
               {selectedRows}
-              <svg xmlns="http://www.w3.org/2000/svg" width="{16}" height="{16}" viewBox="0 0 20 20" fill="none" stroke="currentColor" strokewidth="{2}" strokelinecap="round" strokelinejoin="round" classname="icon icon-tabler icons-tabler-outline icon-tabler-chevron-down"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6 9l6 6l6 -6" /></svg>
-
+              <svg xmlns="http://www.w3.org/2000/svg" width={18} height={18} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-down"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6 9l6 6l6 -6" /></svg>
             </button>
             {isOpen && (
               <div className=" absolute rounded-md shadow-lg bg-white ">
